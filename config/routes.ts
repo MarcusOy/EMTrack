c8b2f46e1be41427c07bb5283438ba0c027e6db3
Shamari Walker <shamariwalkerjr17@gmail.com>
2021-03-19 19:32:42 -0400

    "Created patient care page to take in patient information and display it in a table."

--- config/routes.ts
+++ config/routes.ts
@@ -1,68 +1,74 @@
 ﻿export default [
-  {
-    path: '/user',
-    layout: false,
-    routes: [
-      {
-        path: '/user',
-        routes: [
-          {
-            name: 'login',
-            path: '/user/login',
-            component: './User/login',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    path: '/welcome',
-    name: 'Home',
-    icon: 'home',
-    component: './Welcome',
-  },
-  {
-    path: '/admin',
-    name: 'admin',
-    icon: 'crown',
-    access: 'canAdmin',
-    component: './Admin',
-    routes: [
-      {
-        path: '/admin/sub-page',
-        name: 'sub-page',
-        icon: 'smile',
-        component: './Welcome',
-      },
-    ],
-  },
-  {
-    name: 'list.table-list',
-    icon: 'table',
-    path: '/list',
-    component: './TableList',
-    access: 'canAdmin'
-  },
-  {
-    name: 'Calls',
-    path: '/calls',
-    component: './Call',
-    icon: 'PhoneOutlined',
-    access: 'isOperator'
-
-  },
-  {
-    name: 'Emergencies',
-    path: '/emergencies',
-    component: './Emergency',
-    icon: 'WarningOutlined',
-    access: 'isOperator'
-  },
-  {
-    path: '/',
-    redirect: '/welcome',
-  },
-  {
-    component: './404',
-  },
+	{
+		path: '/user',
+		layout: false,
+		routes: [
+			{
+				path: '/user',
+				routes: [
+					{
+						name: 'login',
+						path: '/user/login',
+						component: './User/login',
+					},
+				],
+			},
+		],
+	},
+	{
+		path: '/welcome',
+		name: 'Home',
+		icon: 'home',
+		component: './Welcome',
+	},
+	{
+		path: '/admin',
+		name: 'admin',
+		icon: 'crown',
+		access: 'canAdmin',
+		component: './Admin',
+		routes: [
+			{
+				path: '/admin/sub-page',
+				name: 'sub-page',
+				icon: 'smile',
+				component: './Welcome',
+			},
+		],
+	},
+	{
+		name: 'list.table-list',
+		icon: 'table',
+		path: '/list',
+		component: './TableList',
+		access: 'canAdmin',
+	},
+	{
+		name: 'Calls',
+		path: '/calls',
+		component: './Call',
+		icon: 'PhoneOutlined',
+		access: 'isOperator',
+	},
+	{
+		name: 'Emergencies',
+		path: '/emergencies',
+		component: './Emergency',
+		icon: 'WarningOutlined',
+		access: 'isOperator',
+	},
+	{
+		name: 'Patients',
+		path: '/patients',
+		component: './Patient',
+		icon: 'WarningOutlined',
+		access: 'isEMT',
+	},
+	{
+		path: '/',
+		redirect: '/welcome',
+	},
+	{
+		component: './404',
+	},
 ];
